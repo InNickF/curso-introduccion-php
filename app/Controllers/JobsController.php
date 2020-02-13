c2b76ade7cdbc500ac216f23d087c6a8f14334ee
Nick <in.nickf@gmail.com>
2020-02-13 16:50:00 -0500

    Se agregó el respect/validator para validar los campos de los form de agregar Job y project

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -4,18 +4,35 @@ namespace App\Controllers;
 
 use App\Models\Job;
 use App\Controllers\BaseController;
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController {
     public function getAddJobAction($request) {
-
-        if($request->getMethod() == 'POST') {
+        $getMessage = null;
+        $e = null;
+        $saved = null;
+        if ($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
-            $job = new Job();
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->save();
+            $jobValidator = v::key('title', v::stringType()->notEmpty())
+                ->key('description', v::stringType()->notEmpty());
+
+            try {
+                $jobValidator->assert($postData);
+                $job = new Job();
+                $job->title = $postData['title'];
+                $job->description = $postData['description'];
+                $job->save();
+                $saved = true;
+                $getMessage = 'Se creó correctamente el nuevo Job.';
+            } catch (\Exception $e) {
+                $getMessage = 'Ha ocurrido un error al enviar el formulario.';
+            };
         };
 
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', [
+            'getMessage' => $getMessage,
+            'error' => $e,
+            'saved' => $saved,
+            ]);
     }
 }
--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -1,22 +1,42 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Controllers\BaseController;
 use App\Models\Project;
+use Respect\Validation\Validator as v;
 
 class ProjectsController extends BaseController {
     public function getAddProjectAction($request) {
+        $e = null;
+        $saved = null;
+        $getMessage = null;
+        
+        $postData = $request->getParsedBody();
+        $projectValidator = v::key('title', v::stringType()->notEmpty())
+            ->key('description', v::stringType()->notEmpty());
+        
+        if ($request->getMethod() == 'POST') {
 
-
-        if($request->getMethod() == 'POST') {
-            $postData = $request->getParsedBody();
-            $project = new Project();
-            $project->title = $postData['title'];
-            $project->description = $postData['description'];
-            $project->save();
+            try {
+                $projectValidator->assert($postData);
+                $project = new Project();
+                $project->title = $postData['title'];
+                $project->description = $postData['description'];
+                $project->save();
+                $saved = true;
+                $getMessage = 'Se ha creado correctamente el Project.';
+                
+            } catch (\Exception $e) {
+                $getMessage = 'Ha ocurrido un error al tratar de crear el Project, corrige tu formulario y vuelve a intentar.';
+            }
         };
 
-        return $this->renderHTML('addProject.twig');
+        return $this->renderHTML('addProject.twig', [
+            'error' => $e,
+            'getMessage' => $getMessage,
+            'saved' => $saved,
+        ]);
         // include_once '../views/addJob.php';
     }
 }
