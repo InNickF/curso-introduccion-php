e59a12369c1bbfabc345eaf45f380528e799b4d8
In Nick Fuenmayor <in.nickf@gmail.com>
2020-02-18 18:18:12 -0500

    We added Phinx for db migrations

---
+++ db/migrations/20200218211948_create_jobs_table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Phinx\Db\Action\AddColumn;
+use Phinx\Migration\AbstractMigration;
+
+class CreateJobsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $existJobsTable = $this->hasTable('jobs');
+        if(!$existJobsTable) {
+            $jobTable = $this->table('jobs');
+            $jobTable->addColumn('title', 'string')
+            ->addColumn('description', 'string')
+            ->addColumn('months', 'intiger')
+            ->addColumn('logo', 'string')
+            ->addColumn('visible', 'boolean')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addColumn('deleted_at', 'datetime')
+            ->create();
+        }
+
+        $existProjectsTable = $this->hasTable('projects');
+        if(!$existProjectsTable) {
+            $projectTable = $this->table('projects');
+            $projectTable->addColumn('title', 'string')
+            ->addColumn('description', 'string')
+            ->addColumn('logo', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addColumn('deleted_at', 'datetime')
+            ->create();
+        }
+
+        $existUsersTable = $this->hasTable('users');
+        if(!$existUsersTable) {
+            $usersTable = $this->table('users');
+            $usersTable->addColumn('name', 'string')
+            ->addColumn('username', 'string', ['limit' => 40])
+            ->addColumn('email', 'string', ['limit' => 40])
+            ->addColumn('password', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addColumn('deleted_at', 'datetime')
+            ->addIndex(['username', 'email'], ['unique' => true])
+            ->create();
+        }
+
+    }
+}
